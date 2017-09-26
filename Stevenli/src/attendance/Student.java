27865386297ddd4aedfabb5455b984d966c185ee
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-09-26 11:45:17 -0400

    a

---
+++ Stevenli/src/attendance/Student.java
@@ -0,0 +1,48 @@
+package attendance;
+
+public class Student implements Attendee {
+	private String first;
+	private String last;
+	private boolean present;
+	public Student(String first, String last) {
+		this.first=first;
+		this.last=last;
+		present=false;
+	}
+	public boolean isPresent() {
+		return present;
+	}
+	public void setPresent(boolean present) {
+		this.present=present;
+	}
+	public String getFirstName() {
+		return first;
+	}
+	public String getLastName() {
+		return last;
+	}
+	public boolean mathces(String first, String last) {
+		return (first.toLowerCase().equals(this.first.toLowerCase())&& last.toLowerCase().equals(this.last.toLowerCase()));
+	}
+	public boolean matches(String last) {
+		return(last.toLowerCase().equals(this.last.toLowerCase()));
+	}
+	public String getReportString() {
+		String temp1=last;
+		String temp2=first;
+		if(last.length()>20) {
+			temp1=last.substring(0, 17)+"...";
+		}
+		if(first.length()>20) {
+			temp2=first.substring(0, 17)+"...";
+		}
+		while(temp1.length()<20) {
+			temp1+=" ";
+		}
+		temp1+=temp2;
+		while(temp1.length()<40) {
+			temp1+=" ";
+		}
+		return temp1+=present;
+	}
+}

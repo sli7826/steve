2924b3f785f5e6f3668342694abd4251946bed30
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-09-28 11:45:07 -0400

    a

---
+++ Stevenli/src/chatbotProject/Chatbot.java
@@ -0,0 +1,30 @@
+package chatbotProject;
+
+public class Chatbot {
+	private String userName;
+	private Topic steven;
+	private boolean chatting;
+
+	public Chatbot() {
+		steven = new Chatbot();
+		userName="unknown";
+		chatting=true;
+		
+	}
+	public void startTalking() {
+		ChatbotMain.print("welcome to our chatbot! What is your name?");
+		userName=ChatbotMain.getInput();
+		while(chatting) {
+			ChatbotMain.print("What do you want to talk aabout?");
+			String response = ChatbotMain.getInput();
+			if(steven.IsTriggered(response)) {
+				chatting = false;
+				steven.startChatting();
+			}
+			else {
+				ChatbotMain.print("a");
+			}
+		}
+	}
+
+}

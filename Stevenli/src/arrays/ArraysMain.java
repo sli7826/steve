ff71561b58f2603d1e11496ef950b34931b0cc9a
BT_1N3_07 <BT_1N3_07@BT_1N3_07.bktech.local>
2017-10-16 11:43:18 -0400

    a

--- Stevenli/src/arrays/ArraysMain.java
+++ Stevenli/src/arrays/ArraysMain.java
@@ -6,14 +6,48 @@ public class ArraysMain {
 	private String[] testArray;
 	private int[] intRay;
 	public ArraysMain() {
-		intRay=new int[50];
-		populate(intRay);
+		intRay=new int[52];
+		//populate(intRay);
+		//checkOccurences(intRay,3,18);
+		populate1ToN(intRay);
+		shuffle(intRay);
 		System.out.println(Arrays.toString(intRay));
+	}
+
+	private void shuffle(int[] arr) {
+		for(int i=0;i<500;i++) {
+			swap(arr,(int)(Math.random()*arr.length),(int)(Math.random()*arr.length));
+		}
+		
+	}
+
+	private void swap(int[] arr, int i, int j) {
+		int a=arr[i];
+		arr[i]=arr[j];
+		arr[j]=a;
+		
+	}
+
+	private void populate1ToN(int[] arr) {
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=i+1;
+		}
+		
+	}
+
+	private void checkOccurences(int[] arr, int start, int end) {
+		int[] counter=new int[end-start+1];
+		for(int value:intRay) {
+			counter[value-start]++;
+		}
+		for(int i=0;i<counter.length;i++) {
+			System.out.println("A "+(start+i)+" was rolled "+counter[i]+" times.");
+		}
 	}
 
 	private void populate(int[] intRay) {
 		for(int i=0;i<intRay.length;i++) {
-			intRay[i]=diceRoll(2);
+			intRay[i]=diceRoll(3);
 		}
 	}
 
